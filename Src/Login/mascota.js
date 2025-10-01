4f021cb38e13c32e21602b3cef877852d8f4fc16
daponcer-dev <daponcer.dev@gmail.com>
2025-09-30 19:24:47 -0500

    Update mascota.js

--- Src/Login/mascota.js
+++ Src/Login/mascota.js
@@ -1,12 +1,14 @@
-// Cuando el usuario enfoca un input
 const inputs = document.querySelectorAll(".input-group input");
 const muñeco = document.getElementById("muñequito");
+const mascotaBox = document.querySelector(".mascota .msg");
 
 inputs.forEach(input => {
   input.addEventListener("focus", () => {
     muñeco.classList.add("awake");
+    mascotaBox.textContent = "🙌 ¡Listo para ayudarte!";
   });
   input.addEventListener("blur", () => {
     muñeco.classList.remove("awake");
+    mascotaBox.textContent = "💤 Estoy durmiendo...";
   });
 });
